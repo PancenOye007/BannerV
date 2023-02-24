181a32066a93e4dacfdb60cb22aab7b6ee80261b
PancenOye007 <115642214+PancenOye007@users.noreply.github.com>
2023-02-24 08:10:13 +0700

    Create WH160x600.js

---
+++ WH160x600.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '2c59546347701c270c5a9e701a6a3e2a',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/2c59546347701c270c5a9e701a6a3e2a/invoke.js"></scr' + 'ipt>');
